debc3915bc5bc838e755e317a4682b43148f4a06
DocenkoG <docn@mail.ru>
2019-07-01 14:19:35 +0300

    Добавлена конвертация второго файла (без скачивания)

--- auvix.py
+++ auvix.py
@@ -9,7 +9,7 @@ import time
 import shutil
 import openpyxl                      # Для .xlsx
 #import xlrd                          # для .xls
-from   price_tools import getCellXlsx, getCell, quoted, dump_cell, currencyType, openX, sheetByName
+from   price_tools import getCellXlsx, getCell, nameToId, currencyType, openX, sheetByName
 import csv
 import requests, lxml.html
 
@@ -98,6 +98,107 @@ def convert_csv2csv( cfg ):
     outFileUSD.close()
 
 
+def convert_excel2csv(cfg):
+    csvFName = cfg.get('basic', 'filename_out')
+    priceFName = cfg.get('basic', 'filename_in')
+    sheetName = cfg.get('basic', 'sheetname')
+
+    log.debug('Reading file ' + priceFName)
+    sheet = sheetByName(fileName=priceFName, sheetName=sheetName)
+    if not sheet:
+        log.error("Нет листа " + sheetName + " в файле " + priceFName)
+        return False
+    log.debug("Sheet   " + sheetName)
+    out_cols = cfg.options("cols_out")
+    in_cols = cfg.options("cols_in")
+    out_template = {}
+    for vName in out_cols:
+        out_template[vName] = cfg.get("cols_out", vName)
+    in_cols_j = {}
+    for vName in in_cols:
+        in_cols_j[vName] = cfg.getint("cols_in", vName)
+    # brands,   discount     = config_read(cfgFName, 'discount')
+    # for k in discount.keys():
+    #    discount[k] = (100 - int(discount[k]))/100
+    # print(discount)
+
+    outFile = open(csvFName, 'w', newline='', encoding='CP1251', errors='replace')
+    csvWriter = csv.DictWriter(outFile, fieldnames=out_cols)
+    csvWriter.writeheader()
+
+    '''                                     # Блок проверки свойств для распознавания групп      XLSX                                  
+    for i in range(2393, 2397):                                                         
+        i_last = i
+        ccc = sheet.cell( row=i, column=in_cols_j['группа'] )
+        print(i, ccc.value)
+        print(ccc.font.name, ccc.font.sz, ccc.font.b, ccc.font.i, ccc.font.color.rgb, '------', ccc.fill.fgColor.rgb)
+        print('------')
+    '''
+    '''                                     # Блок проверки свойств для распознавания групп      XLS                                  
+    for i in range(0, 75):                                                         
+        xfx = sheet.cell_xf_index(i, 0)
+        xf  = book.xf_list[xfx]
+        bgci  = xf.background.pattern_colour_index
+        fonti = xf.font_index
+        ccc = sheet.cell(i, 0)
+        if ccc.value == None :
+            print (i, colSGrp, 'Пусто!!!')
+            continue
+                                         # Атрибуты шрифта для настройки конфига
+        font = book.font_list[fonti]
+        print( '---------------------- Строка', i, '-----------------------', sheet.cell(i, 0).value)
+        print( 'background_colour_index=',bgci)
+        print( 'fonti=', fonti, '           xf.alignment.indent_level=', xf.alignment.indent_level)
+        print( 'bold=', font.bold)
+        print( 'weight=', font.weight)
+        print( 'height=', font.height)
+        print( 'italic=', font.italic)
+        print( 'colour_index=', font.colour_index )
+        print( 'name=', font.name)
+    return
+    '''
+
+    recOut = {}
+    for i in range(1, sheet.max_row + 1):  # xlsx
+        #   for i in range(1, sheet.nrows) :                                     # xls
+        i_last = i
+        try:
+            impValues = getXlsxString(sheet, i, in_cols_j)  # xlsx
+            #           impValues = getXlsString(sheet, i, in_cols_j)                # xls
+            # print( impValues )
+            if impValues['цена1'] == '0':  # (ccc.value == None) or (ccc2.value == None) :     # Пустая строка
+                pass
+                # print( 'Пустая строка. i=',i, impValues )
+            elif impValues['код_'] == '' or impValues['код_'] == 'Арт.':  # Пустая строка
+                print(i, 'Пусто!!!')
+                continue
+            else:  # Обычная строка
+                for outColName in out_template.keys():
+                    shablon = out_template[outColName]
+                    for key in impValues.keys():
+                        if shablon.find(key) >= 0:
+                            shablon = shablon.replace(key, impValues[key])
+                    if (outColName == 'закупка') and ('*' in shablon):
+                        p = shablon.find("*")
+                        vvv1 = float(shablon[:p])
+                        vvv2 = float(shablon[p + 1:])
+                        shablon = str(round(vvv1 * vvv2, 2))
+                    recOut[outColName] = shablon.strip()
+
+                recOut['код'] = nameToId(recOut['код'])
+                csvWriter.writerow(recOut)
+
+        except Exception as e:
+            print(e)
+            if str(e) == "'NoneType' object has no attribute 'rgb'":
+                pass
+            else:
+                log.debug('Exception: <' + str(e) + '> при обработке строки ' + str(i) + '.')
+
+    log.info('Обработано ' + str(i_last) + ' строк.')
+    outFile.close()
+
+
 
 def config_read( cfgFName ):
     cfg = configparser.ConfigParser(inline_comment_prefixes=('#'))
@@ -115,8 +216,8 @@ def download( cfg ):
     retCode     = False
     filename_new= cfg.get('download','filename_new')
     filename_old= cfg.get('download','filename_old')
-    login       = cfg.get('download','login'    )
-    password    = cfg.get('download','password' )
+    login       = cfg.get('private','login'    )
+    password    = cfg.get('private','password' )
     url_lk      = cfg.get('download','url_lk'   )
     url_file    = cfg.get('download','url_file' )
     headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.0; rv:14.0) Gecko/20100101 Firefox/14.0.1',
@@ -202,20 +303,30 @@ def main(dealerName):
     '''
     if os.path.exists('getting.cfg'):
         cfg = config_read('getting.cfg')
-        filename_new_1 = cfg.get('basic','filename_new_1')
-        filename_new_2 = cfg.get('basic','filename_new_2')
+        filename_new = cfg.get('basic','filename_new')
         if cfg.has_section('download'):
             rc_download = download(cfg)
-        if not(rc_download==True or is_file_fresh( filename_new_1, int(cfg.get('basic','срок годности')))):
+        if not(rc_download==True or is_file_fresh( filename_new, int(cfg.get('basic','срок годности')))):
             return False
     for cfgFName in os.listdir("."):
         if cfgFName.startswith("cfg") and cfgFName.endswith(".cfg"):
             log.info('----------------------- Processing '+cfgFName )
             cfg = config_read(cfgFName)
+            rc_download = False
+            if cfg.has_section('download'):
+                rc_download = download(cfg)
             filename_in = cfg.get('basic','filename_in')
-            if rc_download==True or is_file_fresh( filename_in, int(cfg.get('basic','срок годности'))):
-                convert_excel2csv(cfg)
+            if not (rc_download is True or
+                    not cfg.has_option('basic','срок годности') or
+                    is_file_fresh(filename_in, int(cfg.get('basic', 'срок годности')))):
+                return False
 
+            if filename_in == 'Price_AUVIX_dealer_csv.csv':
+                convert_csv2csv(cfg)
+            elif filename_in == 'new_amx_ddp_msk.xlsx':
+                convert_excel2csv(cfg)
+            else:
+                log.error('Не ожидаемый файл %s', filename_in)
 
 if __name__ == '__main__':
     myName = os.path.basename(os.path.splitext(sys.argv[0])[0])
